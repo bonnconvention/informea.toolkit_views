0804bb7998165c96b3934e9a5578cc61e311af07
Cristian Romanescu <cristian@romanescu.ro>
2014-04-11 14:17:42 +0300

    Migrated the eurobats views after refactoring

--- eurobats_toolkit_views.sql
+++ eurobats_toolkit_views.sql
@@ -1,57 +1,67 @@
 -- Meetings
-CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_meetings` AS
+
+-- informea_meetings
+CREATE OR REPLACE DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_meetings` AS
     SELECT
         a.uuid AS id,
         LOWER(instr_name.title) AS treaty,
-        'http://www.eurobats.org/official_documents/meeting_of_parties' AS url,
+        CONCAT('http://www.eurobats.org/node/', a.nid) AS url,
         b.field_meeting_start_value AS `start`,
         c.field_meeting_end_value AS `end`,
         NULL AS repetition,
-        LOWER(e.name) AS `kind`,
-        LOWER(g.name) AS `type`,
-        NULL AS `access`,
-        LOWER(i.name) AS `status`,
+        LOWER(d1.name) AS kind,
+        LOWER(e1.name) AS `type`,
+        NULL AS access,
+        LOWER(f1.name) AS `status`,
         NULL AS imageUrl,
         NULL AS imageCopyright,
-        j.field_meeting_location_value AS `location`,
-        k.field_meeting_city_value AS `city`,
-        n.field_country_iso2_value AS `country`,
-        o.field_meeting_latitude_value AS `latitude`,
-        p.field_meeting_longitude_value AS `longitude`,
-        FROM_UNIXTIME(a.changed, '%Y-%m-%d %H:%i:%s') AS updated
-    FROM node a
-    INNER JOIN field_data_field_meeting_instrument instr ON a.nid = instr.entity_id
-    LEFT JOIN node instr_name ON instr.field_meeting_instrument_target_id = instr_name.nid
-    INNER JOIN field_data_field_meeting_start b ON a.nid = b.entity_id
-    LEFT JOIN field_data_field_meeting_end c ON a.nid = c.entity_id
-    LEFT JOIN field_data_field_meeting_kind d ON a.nid = d.entity_id
-    LEFT JOIN taxonomy_term_data e ON d.field_meeting_kind_tid = e.tid
-    INNER JOIN field_data_field_meeting_type f ON a.nid = f.entity_id
-    INNER JOIN taxonomy_term_data g ON f.field_meeting_type_tid = g.tid
-    LEFT JOIN field_data_field_meeting_status h ON a.nid = h.entity_id
-    LEFT JOIN taxonomy_term_data i ON h.field_meeting_status_tid = i.tid
-    LEFT JOIN field_revision_field_meeting_location j ON a.nid = j.entity_id
-    LEFT JOIN field_data_field_meeting_city k ON a.nid = k.entity_id
-    INNER JOIN field_data_field_meeting_country m ON a.nid = m.entity_id
-    INNER JOIN field_data_field_country_iso2 n ON m.field_meeting_country_target_id = n.entity_id
-    LEFT JOIN field_data_field_meeting_latitude o ON a.nid = o.entity_id
-    LEFT JOIN field_data_field_meeting_longitude p ON a.nid = o.entity_id
-    WHERE a.`type` = 'meeting'
-        AND LOWER(g.name) IN ('mop', 'cop')
-        AND LOWER(instr_name.title) IN ('eurobats');
+        g.field_meeting_location_value AS location,
+        h.field_meeting_city_value AS city,
+        i1.field_country_iso2_value AS country,
+        j.field_meeting_latitude_value AS `latitude`,
+        k.field_meeting_longitude_value AS `longitude`,
+        date_format(from_unixtime(a.changed),'%Y-%m-%d %H:%i:%s') AS updated
+    FROM
+        node a
+        INNER JOIN field_data_field_instrument instr ON a.nid = instr.entity_id
+        LEFT JOIN node instr_name ON instr.field_instrument_target_id = instr_name.nid
+        INNER JOIN field_data_field_meeting_start b ON a.nid = b.entity_id
+        LEFT JOIN field_data_field_meeting_end c ON a.nid = c.entity_id
+        LEFT JOIN field_data_field_meeting_kind d ON a.nid = d.entity_id
+        LEFT JOIN taxonomy_term_data d1 ON d.field_meeting_kind_tid = d1.tid
+        INNER JOIN field_data_field_meeting_type e ON a.nid = e.entity_id
+        INNER JOIN taxonomy_term_data e1 ON e.field_meeting_type_tid = e1.tid
+        LEFT JOIN field_data_field_meeting_status f ON a.nid = f.entity_id
+        LEFT JOIN taxonomy_term_data f1 ON f.field_meeting_status_tid = f1.tid
+        LEFT JOIN field_revision_field_meeting_location g ON a.nid = g.entity_id
+        LEFT JOIN field_data_field_meeting_city h ON a.nid = h.entity_id
+        INNER JOIN field_data_field_country i ON a.nid = i.entity_id
+        INNER JOIN field_data_field_country_iso2 i1 ON i.field_country_target_id = i1.entity_id
+        LEFT JOIN field_data_field_meeting_latitude j ON a.nid = j.entity_id
+        LEFT JOIN field_data_field_meeting_longitude k ON a.nid = k.entity_id
+    WHERE
+        a.`type` = 'meeting'
+        AND LOWER(e1.name) IN ('cop', 'mop')
+        AND LOWER(instr_name.title) = 'eurobats'
+    GROUP BY a.uuid;
 
---
-CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_meetings_description` AS
+
+-- informea_meetings_description
+CREATE OR REPLACE DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_meetings_description` AS
     SELECT
         CONCAT(a.uuid, '-en') AS id,
         a.uuid AS meeting_id,
         'en' AS `language`,
-        b.field_meeting_description_value AS description
+        b.body_value AS description
     FROM node a
-    INNER JOIN field_data_field_meeting_description b ON a.nid = b.entity_id;
+        INNER JOIN field_data_body b ON a.nid = b.entity_id
+        WHERE
+            b.body_value IS NOT NULL
+            AND TRIM(b.body_value) <> '';
 
 
-CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_meetings_title` AS
+-- informea_meetings_title
+CREATE OR REPLACE DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_meetings_title` AS
     SELECT
         CONCAT(a.uuid, '-en') AS id,
         a.uuid AS meeting_id,
@@ -59,13 +69,14 @@ CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`edw_www`@`localhost` SQL SECURITY
         a.title
     FROM node a WHERE a.`type` = 'meeting';
 
+
 -- DECISIONS
 
--- Create a support view with COP meetings and their decision IDs
-CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_decisions_cop_documents` AS
+-- informea_decisions_cop_documents - Support view with COP meetings and their documents IDs
+CREATE OR REPLACE DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_decisions_cop_documents` AS
     SELECT
-        h.entity_id AS id_document,
-        a.uuid AS id_meeting
+        a.uuid AS id_meeting,
+        h.entity_id AS id_document
     FROM
         node a
         INNER JOIN field_data_field_meeting_type f ON a.nid = f.entity_id
@@ -75,10 +86,12 @@ CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`edw_www`@`localhost` SQL SECURITY
         a.type = 'meeting'
         AND LOWER(g.name) IN ('cop', 'mop');
 
-CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_decisions` AS
+
+-- informea_decisions
+CREATE OR REPLACE DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_decisions` AS
     SELECT
         a.uuid AS id,
-        'http://www.eurobats.org/official_documents/meeting_of_parties' AS link,
+        CONCAT('http://www.eurobats.org/node/', a.nid) AS link,
         b1.name AS `type`,
         c1.name AS `status`,
         d.field_document_number_value AS number,
@@ -94,28 +107,30 @@ CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`edw_www`@`localhost` SQL SECURITY
         INNER JOIN field_data_field_document_status c ON c.entity_id = a.nid
         INNER JOIN taxonomy_term_data c1 ON c.field_document_status_tid = c1.tid
         INNER JOIN field_data_field_document_number d ON d.entity_id = a.nid
-        INNER JOIN field_data_field_document_instrument e ON e.entity_id = a.nid
-        INNER JOIN node e1 ON e.field_document_instrument_target_id = e1.nid
+        INNER JOIN field_data_field_instrument e ON e.entity_id = a.nid
+        INNER JOIN node e1 ON e.field_instrument_target_id = e1.nid
         INNER JOIN field_data_field_document_publish_date f ON f.entity_id = a.nid
         INNER JOIN informea_decisions_cop_documents g ON g.id_document = a.nid
     WHERE
         a.`type`='document'
-        AND LOWER(b1.name) IN ('recommendation', 'resolution', 'decision')
+        AND LOWER(b1.name) IN ('resolution', 'recommendation', 'decision')
         AND LOWER (e1.title) IN ('eurobats');
 
---
-CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_decisions_content` AS
+
+-- informea_decisions_content
+CREATE OR REPLACE DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_decisions_content` AS
     SELECT
         NULL AS id, NULL AS decision_id, NULL AS `language`, NULL AS content
     LIMIT 0;
 
---
-CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_decisions_documents` AS
+
+-- informea_decisions_documents
+CREATE OR REPLACE DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_decisions_documents` AS
     SELECT
         CONCAT(a.uuid, '-', f2.fid) AS id,
         a.uuid AS decision_id,
         CONCAT('/var/local/eurobats/www/sites/default/files/', REPLACE(f2.uri, 'public://', '')) AS diskPath,
-        CONCAT('http://eurobats.eaudeweb.ro/sites/default/files/', REPLACE(f2.uri, 'public://', '')) AS url,
+        CONCAT('http://www.eurobats.org/sites/default/files/', REPLACE(f2.uri, 'public://', '')) AS url,
         f2.filemime AS mimeType,
         f1.`language` AS language,
         f2.filename AS filename
@@ -125,8 +140,8 @@ CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`edw_www`@`localhost` SQL SECURITY
         INNER JOIN field_data_field_document_status c ON c.entity_id = a.nid
         INNER JOIN taxonomy_term_data c1 ON c.field_document_status_tid = c1.tid
         INNER JOIN field_data_field_document_number d ON d.entity_id = a.nid
-        INNER JOIN field_data_field_document_instrument e ON e.entity_id = a.nid
-        INNER JOIN node e1 ON e.field_document_instrument_target_id = e1.nid
+        INNER JOIN field_data_field_instrument e ON e.entity_id = a.nid
+        INNER JOIN node e1 ON e.field_instrument_target_id = e1.nid
         INNER JOIN field_data_field_document_files f ON f.entity_id = a.nid
         INNER JOIN field_data_field_document_file f1 ON f1.entity_id = f.field_document_files_value
         INNER JOIN file_managed f2 ON f2.fid = f1.field_document_file_fid
@@ -136,26 +151,30 @@ CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`edw_www`@`localhost` SQL SECURITY
         AND LOWER (e1.title) IN ('eurobats')
         AND f2.filemime IN ('application/pdf', 'application/msword');
 
---
-CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_decisions_keywords` AS
+
+-- informea_decisions_keywords
+CREATE OR REPLACE DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_decisions_keywords` AS
     SELECT
         NULL AS id, NULL AS decision_id, NULL AS `namespace`, NULL AS term
     LIMIT 0;
 
---
-CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_decisions_longtitle` AS
+
+-- informea_decisions_longtitle
+CREATE OR REPLACE DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_decisions_longtitle` AS
     SELECT
         NULL AS id, NULL AS decision_id, NULL AS `language`, NULL AS long_title
     LIMIT 0;
 
---
-CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_decisions_summary` AS
+
+-- informea_decisions_summary
+CREATE OR REPLACE DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_decisions_summary` AS
     SELECT
         NULL AS id, NULL AS decision_id, NULL AS language, NULL AS summary
     LIMIT 0;
 
---
-  CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_decisions_title` AS
+
+-- informea_decisions_title
+CREATE OR REPLACE DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_decisions_title` AS
     SELECT
         CONCAT(a.uuid, '-', 'en') AS id,
         a.uuid AS decision_id,
@@ -167,9 +186,44 @@ CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`edw_www`@`localhost` SQL SECURITY
         INNER JOIN field_data_field_document_status c ON c.entity_id = a.nid
         INNER JOIN taxonomy_term_data c1 ON c.field_document_status_tid = c1.tid
         INNER JOIN field_data_field_document_number d ON d.entity_id = a.nid
-        INNER JOIN field_data_field_document_instrument e ON e.entity_id = a.nid
+        INNER JOIN field_data_field_instrument e ON e.entity_id = a.nid
         INNER JOIN node e1 ON e.field_document_instrument_target_id = e1.nid
     WHERE
         a.`type`='document'
         AND LOWER(b1.name) IN ('resolution', 'recommendation', 'decision')
         AND LOWER (e1.title) IN ('eurobats');
+
+
+-- COUNTRY REPORTS (National Reports)
+
+-- informea_country_reports
+CREATE OR REPLACE DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_country_reports` AS
+    SELECT
+        a.uuid AS id,
+        'eurobats' AS treaty,
+        UPPER(h.field_country_iso3_value) AS country,
+        f.field_document_publish_date_value AS submission,
+        CONCAT('http://www.eurobats.org/node/', a.nid) AS url,
+        date_format(from_unixtime(a.created),'%Y-%m-%d %H:%i:%s') AS updated
+    FROM node a
+        INNER JOIN field_data_field_document_type b ON b.entity_id = a.nid
+        INNER JOIN taxonomy_term_data b1 ON b.field_document_type_tid = b1.tid
+        INNER JOIN field_data_field_instrument e ON e.entity_id = a.nid
+        INNER JOIN node e1 ON e.field_instrument_target_id = e1.nid
+        INNER JOIN field_data_field_document_publish_date f ON f.entity_id = a.nid
+        INNER JOIN field_data_field_country g ON g.entity_id = a.nid
+        INNER JOIN field_data_field_country_iso3 h ON g.field_country_target_id = h.entity_id
+    WHERE
+        a.`type`='document'
+        AND LOWER(b1.name) = 'national report'
+        AND LOWER (e1.title) IN ('eurobats');
+
+
+-- informea_country_reports_title
+CREATE OR REPLACE DEFINER=`edw_www`@`localhost` SQL SECURITY DEFINER VIEW `informea_country_reports_title` AS
+    SELECT
+        CONCAT(a.uuid, '-en') AS id,
+        a.uuid AS country_report_id,
+        'en' AS 'language',
+        a.title
+    FROM node a WHERE a.`type` = 'document';
